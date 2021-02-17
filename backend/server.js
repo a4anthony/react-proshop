38758ee7797778898311cc827f6df9cab2c592b8
a4anthony <55565463+a4anthony@users.noreply.github.com>
2021-02-17 22:07:43 +0100

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from "path";
+
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -25,6 +27,10 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 app.use(notFound);
 app.use(errorHandler);
 
